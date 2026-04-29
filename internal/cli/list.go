8222a76075ba76c3894aa2a24b1bf99d4af7ade8
Ian Cardona <iancardona.dev@gmail.com>
2026-04-29 23:54:11 +0800

    add list.go

--- internal/cli/list.go
+++ internal/cli/list.go
@@ -1,8 +1,33 @@
 package cli
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+)
 
 func runList(args []string) error {
-	fmt.Println("list called with: ", args)
+	fs := flag.NewFlagSet("list", flag.ContinueOnError)
+	fs.SetOutput(io.Discard)
+
+	format := fs.String("format", "table", "output format: table or json")
+
+	if err := fs.Parse(args); err != nil {
+		return fmt.Errorf("parsing flags: %w", err)
+	}
+
+	if fs.NArg() > 0 {
+		return errors.New("usage: wache list [--format table|json]")
+	}
+
+	switch *format {
+	case "table", "json":
+		// valid
+	default:
+		return fmt.Errorf("invalid format %q: must be 'table' or 'json'", *format)
+	}
+
+	fmt.Printf("list: format=%s\n", *format)
 	return nil
 }
